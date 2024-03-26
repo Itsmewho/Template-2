336222f8bb43b69dd88472aab398c14342bade8e
Itsmewho <57646192+Itsmewho@users.noreply.github.com>
2024-03-26 18:58:40 +0100

    connection db done

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
+
 import fitnessBlog from './data/fitness.js';
 import lifestyles from './data/lifestyle.js';
 import nutritions from './data/nutrition.js';
 
 const port = process.env.PORT || 5000;
+connectDB(); //
 const app = express();
 
 app.get('/', (req, res) => {
